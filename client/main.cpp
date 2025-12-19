8dcf8d0a2cdfbd018b805d57e4ee1c2e028b3c31
SebastLM <sebastmendonca@gmail.com>
2025-12-19 00:36:06 +0000

    forgot to commit client as well :/

--- client/main.cpp
+++ client/main.cpp
@@ -3,19 +3,31 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <fstream>
+#include <unistd.h>
 
-int main() { 
-  
-  struct sockaddr_in serv_addr;
-  const char *hello = "hello from client";
-  char buffer[1024] = {0};
+int main(int argc, char *argv[]) { 
+
+  if (argc < 2) {
+    std::cerr << "usage: ./exec file" << std::endl;
+    return 1;
+  }
+
+  std::ifstream file(argv[1], std::ios::binary);
+  if (!file) {
+    perror("failed to open file");
+    exit(EXIT_FAILURE);
+  }
+
+  // const char *hello = "hello from client";
 
   ssize_t sock = socket(AF_INET, SOCK_STREAM, 0);
   if (sock < 0) {
     perror("error setting up socket\n");
     exit(EXIT_FAILURE);
   }
-
+  
+  struct sockaddr_in serv_addr;
   serv_addr.sin_family = AF_INET;
   inet_pton(AF_INET, "192.168.1.95", &serv_addr.sin_addr);
   serv_addr.sin_port = htons(47195);
@@ -25,11 +37,31 @@ int main() {
     exit(EXIT_FAILURE);
   }
   
-  send(sock, hello, strlen(hello), 0);
-  printf("hello message sent\n");
+  const char* file_name = argv[1];
+  send(sock, file_name, strlen(file_name) + 1, 0); // +1 to guarantee that the file is null terminated \0
+  char buffer[1024*64];
 
-  do {
-    recv(sock, buffer, 1024, 0);
-    printf("%s\n", buffer);
-  } while(1);
+  while (file) {
+    file.read(buffer, sizeof(buffer));
+    std::streamsize n = file.gcount();
+    if (n == 0) break;
+    
+    std::streamsize sent = 0;
+
+    while (sent < n) {
+
+      ssize_t s = send(sock, buffer + sent, n - sent, 0);
+      if (s <= 0) {
+        perror("error sending file chunk");
+        close(sock);
+        file.close();
+        exit(EXIT_FAILURE);
+      }
+      sent += s;
+    }
+  }
+
+  file.close();
+  close(sock);
+  exit(EXIT_SUCCESS);
 }
