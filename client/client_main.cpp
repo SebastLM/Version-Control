9eb52747556e43a3540a1c2c518bc987079d5cad
Sebastião Mendonça <sebastiao.mendonca@tecnico.ulisboa.pt>
2026-09-05 13:28:06 +0100

    fix: removing building index functionality from create

--- client/client_main.cpp
+++ client/client_main.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <string>
 #include <sys/socket.h>
@@ -13,6 +14,59 @@
 #include "send_all_recv_all.h"
 #include "staging.h"
 #include <unistd.h>
+
+#include <arpa/inet.h>
+#include <cstring>
+#include <fcntl.h>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+std::string find_pi_ip() {
+  for (int i = 1; i < 254; ++i) {
+    std::string candidate_ip = "10.55.0." + std::to_string(i);
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+      continue;
+
+    int flags = fcntl(sock, F_GETFL, 0);
+    fcntl(sock, F_SETFL, flags | O_NONBLOCK);
+
+    struct sockaddr_in serv_addr;
+    std::memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(47195);
+    inet_pton(AF_INET, candidate_ip.c_str(), &serv_addr.sin_addr);
+
+    const char *eth_interface = "eth0";
+    setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, eth_interface,
+               strlen(eth_interface));
+
+    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    fd_set fdset;
+    FD_ZERO(&fdset);
+    FD_SET(sock, &fdset);
+
+    struct timeval tv;
+    tv.tv_sec = 0;
+    tv.tv_usec = 10000;
+
+    if (select(sock + 1, NULL, &fdset, NULL, &tv) == 1) {
+      int sockerr;
+      socklen_t len = sizeof(sockerr);
+      getsockopt(sock, SOL_SOCKET, SO_ERROR, &sockerr, &len);
+      if (sockerr == 0) {
+        close(sock);
+        return candidate_ip;
+      }
+    }
+    close(sock);
+  }
+  return "";
+}
 
 int main(int argc, char *argv[]) {
 
@@ -36,9 +90,16 @@ int main(int argc, char *argv[]) {
     add(files);
     return EXIT_SUCCESS;
 
-  } else
+  } else if (strcmp(argv[1], "commit") == 0) {
 
-      if (strcmp(argv[1], "commit") == 0) {
+    std::cout << "finding Raspberry Pi on Ethernet..." << std::endl;
+    std::string pi_ip = find_pi_ip();
+
+    if (pi_ip.empty()) {
+      std::cerr << "Error: Could not find Raspberry Pi on the Ethernet cable."
+                << std::endl;
+      return EXIT_FAILURE;
+    }
 
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
@@ -50,8 +111,7 @@ int main(int argc, char *argv[]) {
     std::memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(47195);
-
-    inet_pton(AF_INET, "ubuntu.local", &serv_addr.sin_addr);
+    inet_pton(AF_INET, pi_ip.c_str(), &serv_addr.sin_addr);
 
     const char *eth_interface = "eth0";
     setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, eth_interface,
@@ -65,8 +125,8 @@ int main(int argc, char *argv[]) {
 
     MsgType protocol = MsgType::COMMIT_FILES;
     uint8_t protocol_tmp = static_cast<uint8_t>(protocol);
-
     send_all(sock, &protocol_tmp, sizeof(protocol_tmp));
+
     commit(sock);
 
     close(sock);
--- client/create_project.cpp
+++ client/create_project.cpp
@@ -6,88 +6,58 @@
 #include "index.h"
 #include "staging.h"
 
-
-
 namespace fs = std::filesystem;
 
-
 int create_project(std::string new_project_name) {
- 
-   
+
   fs::path new_project_root_tmp = fs::current_path();
   std::string new_project_root = new_project_root_tmp.generic_string();
   std::string new_index_path;
 
-  using ProjectIndex = 
-      std::unordered_map<std::string, IndexEntry>;
-    
+  using ProjectIndex = std::unordered_map<std::string, IndexEntry>;
+
   ProjectIndex new_project_index;
-  
+
   bool dot = false;
   try {
 
-    if (new_project_name != ".") { 
+    if (new_project_name != ".") {
 
-      new_project_root = new_project_root + "/" + new_project_name; 
-      
+      new_project_root = new_project_root + "/" + new_project_name;
+
       if (fs::exists(new_project_root)) {
-        std::cerr << "Aborting: Project directory '" << new_project_root 
+        std::cerr << "Aborting: Project directory '" << new_project_root
                   << "' already exists. Use a different name." << std::endl;
         return -1;
-      } 
+      }
 
       new_project_root_tmp = new_project_root;
       fs::create_directories(new_project_root_tmp);
-      
-      
-    } else { 
+
+    } else {
 
       dot = true;
 
-      for (const auto& dirEntry : fs::recursive_directory_iterator(new_project_root_tmp)) {
-        
-        std::string filename = dirEntry.path().filename().string();
-              
-        // in case its already part of a project
-        if (filename.size() > 0 && filename == ".index") {
-          std::cerr << "WARNING: The directory your trying to create a project, already has one attributed to it"
-                    << "\n Manually remove the .index file if you want to create a new project in that same directory" 
-                    << std::endl;
-          return -1;
-        }
-
-        IndexEntry e;
-        e.is_dir = false;
-
-        if (dirEntry.is_directory()) {
-          e.is_dir = true;
-          e.hash = "";
-
-        } else {
-          std::ifstream file(dirEntry.path().string(), std::ios::binary);
-          unsigned char out[32];
-          unsigned int out_len;
-
-          if (hashing(file, out, out_len))
-            e.hash = hash_value_to_store(out, out_len);
-          file.close();
-        }
-        
-        // fs::relative
-        // the path doesnt stay as absolute across filesystems
-        new_project_index[fs::relative(dirEntry.path(), new_project_root_tmp).string()] = e;
-      } 
+      std::string existing_index =
+          new_project_root_tmp.generic_string() + "/.index";
+      if (fs::exists(existing_index)) {
+        std::cerr << "WARNING: The directory you're trying to create a project "
+                     "in already has one.\n"
+                  << "Manually remove the .index file if you want to create a "
+                     "new project in that same directory.\n";
+        return -1;
+      }
     }
 
     new_index_path = new_project_root + "/.index";
 
     save_index(new_index_path, new_project_index, new_project_root);
 
-  } catch (const std::exception& e) { 
+  } catch (const std::exception &e) {
 
     if (!dot)
       fs::remove_all(new_project_root_tmp);
-    
+
     std::cerr << "Failed  to create_project: " << e.what() << std::endl;
     return -1;
   }
